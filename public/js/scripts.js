377ba0dd88fdc7c89aa03e27801aa9628b6fa608
Pratham3shRaut <132664521+Pratham3shRaut@users.noreply.github.com>
2025-01-11 12:49:05 +0530

    Add files via upload

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+// public/js/scripts.js
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    // Add client-side validation if needed
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    // Add client-side validation if needed
+});
